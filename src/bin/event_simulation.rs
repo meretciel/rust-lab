54e3f414b057f804d899e6ed20d32c334c8373d5
meretciel <meretciel.fr@gmail.com>
2025-01-13 21:20:12 -0500

    first working version

--- src/bin/event_simulation.rs
+++ src/bin/event_simulation.rs
@@ -1,13 +1,9 @@
-use std::cell::RefCell;
+
 use std::cmp::{Ordering, Reverse};
 use std::collections::{BinaryHeap, HashMap, VecDeque};
 use std::error::Error;
-use std::rc::Rc;
-use csv::ErrorKind::Serialize;
-use uuid::fmt::Simple;
 use uuid::Uuid;
 use rust_lab::event_sim::{Event, Actor, EventQueue};
-
 
 #[derive(Debug, Copy, Clone)]
 enum EventType {
@@ -18,33 +14,39 @@ enum EventType {
 
 
 #[derive(Debug)]
-struct SimpleEvent {
+struct EventData {
     timestamp: f32,
     event_type: EventType,
     event_id: String,
     destination: String,
 }
 
-impl PartialEq for SimpleEvent {
+impl PartialEq for EventData {
     fn eq(&self, other: &Self) -> bool {
         self.timestamp.eq(&other.timestamp)
     }
 }
 
-impl Eq for SimpleEvent {}
+impl Eq for EventData {}
 
-impl PartialOrd for SimpleEvent {
+impl PartialOrd for EventData {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl Ord for SimpleEvent {
+impl Ord for EventData {
     fn cmp(&self, other: &Self) -> Ordering {
         let result = Reverse(self.timestamp).partial_cmp(&Reverse(other.timestamp));
 
         if let Some(order) = result {
-            return order;
+            if let Ordering::Equal = order {
+                let this_char = self.event_id.chars().next().unwrap();
+                let other_char = other.event_id.chars().next().unwrap();
+                return this_char.cmp(&other_char);
+            } else {
+                return order;
+            }
         } else {
             panic!("Timestamp cannot be NaN.");
         }
@@ -52,9 +54,9 @@ impl Ord for SimpleEvent {
 }
 
 
-impl SimpleEvent {
-    fn new(timestamp: f32, event_type: EventType, destination: String) -> Event {
-        Event {
+impl EventData {
+    fn new(timestamp: f32, event_type: EventType, destination: String) -> EventData {
+        EventData {
             timestamp,
             event_type,
             event_id: Uuid::new_v4().to_string(),
@@ -63,14 +65,14 @@ impl SimpleEvent {
     }
 }
 
-impl Event for SimpleEvent {
-    type EventType = EventType;
+impl Event for EventData {
+    type TyEventType = EventType;
 
     fn timestamp(&self) -> f32 {
         self.timestamp
     }
 
-    fn event_type(&self) -> Self::EventType {
+    fn event_type(&self) -> Self::TyEventType {
         self.event_type
     }
 
@@ -112,35 +114,49 @@ where E: Ord + Event {
     fn pop(&mut self) -> Option<E> {
         self.queue.pop()
     }
+
+    fn is_empty(&self) -> bool {
+        self.queue.is_empty()
+    }
 }
 
 
 struct RequestGenerator {
     address_id: String,
     timestamp: f32,
+    max_timestamp: f32
 }
 
 impl RequestGenerator {
-    fn new(address_id: String, init_timestamp: f32) -> RequestGenerator {
+    fn new(address_id: String, init_timestamp: f32, max_timestamp: f32) -> RequestGenerator {
         RequestGenerator {
             address_id,
             timestamp: init_timestamp,
+            max_timestamp
         }
     }
 }
 
 impl Actor for RequestGenerator {
-    type Event = SimpleEvent;
-    type Queue = SimpleQueue<SimpleEvent>;
+    type TyEvent = EventData;
+    type TyQueue = SimpleQueue<EventData>;
 
     fn name(&self) -> String {
         self.address_id.clone()
     }
 
-    fn handle_event(&mut self, event: Self::Event, event_queue: Self::Queue) {
+    fn handle_event(&mut self, event: Self::TyEvent, event_queue: &mut Self::TyQueue) {
         self.timestamp = event.timestamp;
+        let next_timestamp = self.timestamp + 1.0;
+        
+        if next_timestamp <= self.max_timestamp {
+            event_queue.add(
+                EventData::new(next_timestamp, EventType::Tick, self.address_id.clone())
+            );
+        }
+
         event_queue.add(
-            SimpleEvent::new(self.timestamp + 1.0, EventType::Request, self.address_id.clone())
+            EventData::new(self.timestamp, EventType::Request, "Server_1".to_owned())
         );
     }
 }
@@ -149,59 +165,71 @@ impl Actor for RequestGenerator {
 struct Server {
     address_id: String,
     state: ServerState,
-    queue: VecDeque<SimpleEvent>,
+    queue: VecDeque<EventData>,
     timestamp: f32,
-    next_idle_time: f32
 }
-//
-//
-// impl<Q> Server<Q> {
-//     fn new(address_id: String, init_timestamp: f32) -> Server<Q> {
-//         Server {
-//             address_id,
-//             state: ServerState::Idle,
-//             queue: VecDeque::<Event>::new(),
-//             timestamp: init_timestamp,
-//             next_idle_time: init_timestamp,
-//         }
-//     }
-// }
-//
-// impl<Q> Actor for Server<Q>
-// where Q: EventQueue {
-//     type TypeQueue = Q;
-//
-//     fn handle_event(&mut self, event: Event, event_queue: Self::TypeQueue) {
-//         self.timestamp = event.timestamp;
-//
-//         if let ServerState::Busy = self.state {
-//             if self.timestamp >= self.next_idle_time {
-//                 self.state = ServerState::Idle;
-//                 self.next_idle_time = self.timestamp;
-//             }
-//         }
-//
-//         match self.state {
-//             ServerState::Idle => {
-//                 if let EventType::Request = event.event_type {
-//                     let completion_time = self.timestamp + 10.;
-//                     let next_event = Event::new(
-//                         completion_time,
-//                         EventType::RequestComplete,
-//                         "".to_owned()
-//                     );
-//                     event_queue.add(next_event);
-//                     self.state = ServerState::Busy;
-//                     self.next_idle_time = completion_time;
-//                 }
-//
-//             }
-//             ServerState::Busy => {
-//                 self.queue.push_back(event);
-//             }
-//         }
-//     }
-// }
+
+
+impl Server {
+    fn new(address_id: String, init_timestamp: f32) -> Server {
+        Server {
+            address_id,
+            state: ServerState::Idle,
+            queue: VecDeque::<EventData>::new(),
+            timestamp: init_timestamp,
+            
+        }
+    }
+}
+
+
+impl Actor for Server {
+    type TyEvent = EventData;
+    type TyQueue = SimpleQueue<EventData>;
+
+    fn name(&self) -> String {
+        self.address_id.clone()
+    }
+
+    fn handle_event(&mut self, event: Self::TyEvent, event_queue: &mut Self::TyQueue) {
+        self.timestamp = event.timestamp;
+
+
+        match self.state {
+            ServerState::Idle => {
+                if let EventType::Request = event.event_type {
+                    let completion_time = self.timestamp + 10.;
+                    let next_event = EventData::new(
+                        completion_time,
+                        EventType::RequestComplete,
+                        self.address_id.clone()
+                    );
+                    event_queue.add(next_event);
+                    self.state = ServerState::Busy;
+                }
+
+            }
+            ServerState::Busy => {
+                if let EventType::RequestComplete = event.event_type {
+                    if let Some(queued_event) = self.queue.pop_front() {
+                        let completion_time = self.timestamp + 10.;
+                        let next_event = EventData::new(
+                            completion_time,
+                            EventType::RequestComplete,
+                            self.address_id.clone()
+                        );
+                        event_queue.add(next_event);
+                        self.state = ServerState::Busy;
+                    } else {
+                        self.state = ServerState::Idle;   
+                    }
+                } else {
+                    self.queue.push_back(event);
+                }
+            }
+        }
+    }
+}
 
 
 
@@ -211,10 +239,11 @@ struct Server {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let start_sim_timestamp = 0.0;
-    let end_sim_timestamp = 200.;
-    let mut event_queue = SimpleQueue::<SimpleEvent>::new();
-    let actors: Vec<Box<dyn Actor<Event=SimpleEvent, Queue=SimpleQueue<SimpleEvent>>>> = vec![
-      Box::new(RequestGenerator::new("RequestGenerator".to_owned(), start_sim_timestamp))
+    let end_sim_timestamp = 60.;
+    let mut event_queue = SimpleQueue::<EventData>::new();
+    let actors: Vec<Box<dyn Actor<TyEvent=EventData, TyQueue=SimpleQueue<EventData>>>> = vec![
+      Box::new(RequestGenerator::new("RequestGenerator".to_owned(), start_sim_timestamp, end_sim_timestamp / 2.)),
+      Box::new(Server::new("Server_1".to_owned(), start_sim_timestamp)),
     ];
 
     let mut name_to_actors = HashMap::new();
@@ -223,17 +252,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // Add seed messages.
-    event_queue.add(SimpleEvent::new(
+    event_queue.add(EventData::new(
       start_sim_timestamp,
-      EventType::Request,
+      EventType::Tick,
       "RequestGenerator".to_owned()
     ));
 
     let mut count = 0;
 
-    while count < 1000 {
+    while !event_queue.is_empty() {
         if let Some(event) = event_queue.pop() {
             println!("event: {:?}", event);
+
             let actor = name_to_actors.get_mut(&event.destination);
             if let Some(dest) = actor {
                 dest.handle_event(event, &mut event_queue);
--- src/event_sim.rs
+++ src/event_sim.rs
@@ -1,12 +1,10 @@
-use std::borrow::{Borrow, BorrowMut};
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::borrow::Borrow;
 
 
 pub trait Event {
-    type EventType;
+    type TyEventType;
     fn timestamp(&self) -> f32;
-    fn event_type(&self) -> Self::EventType;
+    fn event_type(&self) -> Self::TyEventType;
     fn event_id(&self) -> String;
     fn destination(&self) -> String;
 }
@@ -14,12 +12,14 @@ pub trait Event {
 pub trait EventQueue<E: Event> {
     fn add(&mut self, event: E);
     fn pop(&mut self) -> Option<E>;
+
+    fn is_empty(&self) -> bool;
 }
 
 pub trait Actor {
-    type Event;
-    type Queue;
+    type TyEvent;
+    type TyQueue;
 
-    fn handle_event(&mut self, event: Self::Event, event_queue: &mut Self::Queue);
+    fn handle_event(&mut self, event: Self::TyEvent, event_queue: &mut Self::TyQueue);
     fn name(&self) -> String;
 }
